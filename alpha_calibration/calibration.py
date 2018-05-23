76874854ed247d1ae320bd3c036e7abab66de7c7
Josh Hooker <joshualeehooker@gmail.com>
2018-05-23 13:47:19 -0500

    Finished alpha calibration

--- alpha_calibration/calibration.py
+++ alpha_calibration/calibration.py
@@ -11,7 +11,7 @@ y = [3183, 5156, 5485, 5805]
 for i in range(len(data)):
   x = [data[i][3], data[i][4], data[i][5], data[i][6]]
   slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-  print data[i][0], data[i][1], data[i][2], slope, intercept
+  print(data[i][0], data[i][1], data[i][2], slope, intercept)
   f.write('%s %s %s %s %s\n' %(data[i][0], data[i][1], data[i][2], slope, intercept))
 
 f.close()
