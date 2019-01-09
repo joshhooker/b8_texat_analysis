d16b849246ea471fa1463c9713357889e33e956b
Josh Hooker <joshualeehooker@gmail.com>
2019-01-09 10:57:30 -0600

    Added plotting

---
+++ azureConversionNew/calcReg3.C
@@ -0,0 +1,77 @@
+#include <cassert>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+#include "CubicSpline.h"
+
+void calcReg3() {
+    std::ifstream in("csReg3.out");
+    assert(in.is_open());
+
+    double var1, var2, var3, var4;
+
+    std::vector<double> cmEnergy_, labAngle_, cmAngle_;
+    while(in >> var1 >> var2 >> var3 >> var4) {
+        cmEnergy_.push_back(var1);
+        labAngle_.push_back(var3);
+        cmAngle_.push_back(var4);
+    }
+    in.close();
+
+    std::ifstream inCS("spectrum_reg3.out");
+    assert(inCS.is_open());
+    std::vector<double> reg3CMEnergy_, reg3CS_, reg3CSErr_;
+    while(inCS >> var1 >> var2 >> var3 >> var4) {
+        reg3CMEnergy_.push_back(var1);
+        reg3CS_.push_back(var2);
+        reg3CSErr_.push_back(var3);
+    }
+    inCS.close();
+
+    CubicSpline splineLabAngle(cmEnergy_, labAngle_);
+    CubicSpline splineCMAngle(cmEnergy_, cmAngle_);
+
+    FILE* file = fopen("azure_reg3.out","w");
+
+    double m1 = 8.;
+    double m2 = 1.;
+
+    double x = m1/m2;
+
+    double crossSection = 1.;
+    double crossSectionErr = 0.1;
+
+    for(Int_t i = 0; i < reg3CMEnergy_.size(); i++) {
+        double cmE = reg3CMEnergy_[i];
+        double cs = reg3CS_[i];
+        double csErr = reg3CSErr_[i];
+
+        double energyBeamReg = cmE*(m1 + m2)/m1;
+        double energyBeamInv = cmE*(m1 + m2)/m2;
+
+        double labAngleLightInv = splineLabAngle(cmE);
+        double labAngleHeavyInv = atan(sin(2.*labAngleLightInv*M_PI/180.)/(x - cos(2.*labAngleLightInv*M_PI/180.)))*180./M_PI;
+
+        double cmAngleLight = splineCMAngle(cmE);
+        double cmAngleHeavy = 3.1415926 - cmAngleLight;
+
+        double factor1 = m2*m2/((m1 + m2)*(m1 + m2));
+        double factor2 = cos(labAngleLightInv*M_PI/180.) + sqrt((m1/m2)*(m1/m2) - sin(labAngleLightInv*M_PI/180.)*sin(labAngleLightInv*M_PI/180.));
+
+        double energyLight = energyBeamReg*factor1*factor2*factor2;
+
+        double labAngleReg = atan(sin(cmAngleLight*M_PI/180.)/(cos(cmAngleLight*M_PI/180.) + m2/m1))*360/(2 * 3.1415926) + 180;
+
+        double crossSectionFactor = pow(sin(labAngleReg*M_PI/180.)/sin(cmAngleLight*M_PI/180.), 2.0)*cos((cmAngleLight - labAngleReg)*M_PI/180.);
+        double crossSectionLab = cs/crossSectionFactor;
+        double crossSectionLabErr = csErr/crossSectionFactor;
+
+        printf("%f %f %f %f %f\n", energyLight, labAngleReg, crossSectionLab, crossSectionLabErr, crossSectionFactor);
+        fprintf(file,"%f %f %f %f\n", energyLight, labAngleReg, crossSectionLab, crossSectionLabErr);
+    }
+
+    fflush(file);
+    fclose(file);
+
+}
---
+++ dissertationPlots/plotReg1.C
@@ -0,0 +1,145 @@
+void plotReg1() {
+    gROOT->SetStyle("Plain");
+
+    double var1, var2, var3, var4, var5, var6, var7, var8, var9;
+
+    std::vector<double> cm_05_25_05_35, exe_05_25_05_35;
+    std::vector<double> fit_05_25_05_35, cs_05_25_05_35, cserr_05_25_05_35;
+    ifstream in_05_25_05_35("9C_Reg1_0.5-_2.5-_0.5+_3.5-.out");
+    while(in_05_25_05_35 >> var1 >> var2 >> var3 >> var4 >> var5 >> var6 >> var7 >> var8 >> var9) {
+        cm_05_25_05_35.push_back(var1);
+        exe_05_25_05_35.push_back(var2);
+        fit_05_25_05_35.push_back(var4);
+        cs_05_25_05_35.push_back(var6);
+        cserr_05_25_05_35.push_back(var7);
+    }
+    in_05_25_05_35.close();
+
+    std::vector<double> cm_05_25_15, exe_05_25_15;
+    std::vector<double> fit_05_25_15;
+    ifstream in_05_25_15("9C_Reg1_0.5-_2.5-_1.5-.out");
+    while(in_05_25_15 >> var1 >> var2 >> var3 >> var4 >> var5 >> var6 >> var7 >> var8 >> var9) {
+        cm_05_25_15.push_back(var1);
+        exe_05_25_15.push_back(var2);
+        fit_05_25_15.push_back(var4);
+    }
+    in_05_25_15.close();
+
+    std::vector<double> cm_05_25_15_35, exe_05_25_15_35;
+    std::vector<double> fit_05_25_15_35;
+    ifstream in_05_25_15_35("9C_Reg1_0.5-_2.5-_1.5+_3.5-.out");
+    while(in_05_25_15_35 >> var1 >> var2 >> var3 >> var4 >> var5 >> var6 >> var7 >> var8 >> var9) {
+        cm_05_25_15_35.push_back(var1);
+        exe_05_25_15_35.push_back(var2);
+        fit_05_25_15_35.push_back(var4);
+    }
+    in_05_25_15_35.close();
+
+    std::vector<double> cm_05_25_25_35, exe_05_25_25_35;
+    std::vector<double> fit_05_25_25_35;
+    ifstream in_05_25_25_35("9C_Reg1_0.5-_2.5-_2.5+_3.5-.out");
+    while(in_05_25_25_35 >> var1 >> var2 >> var3 >> var4 >> var5 >> var6 >> var7 >> var8 >> var9) {
+        cm_05_25_25_35.push_back(var1);
+        exe_05_25_25_35.push_back(var2);
+        fit_05_25_25_35.push_back(var4);
+    }
+    in_05_25_25_35.close();
+
+    std::vector<double> cm_05_25_25, exe_05_25_25;
+    std::vector<double> fit_05_25_25;
+    ifstream in_05_25_25("9C_Reg1_0.5-_2.5-_2.5+.out");
+    while(in_05_25_25 >> var1 >> var2 >> var3 >> var4 >> var5 >> var6 >> var7 >> var8 >> var9) {
+        cm_05_25_25.push_back(var1);
+        exe_05_25_25.push_back(var2);
+        fit_05_25_25.push_back(var4);
+    }
+    in_05_25_25.close();
+
+    std::vector<double> cm_05_25_35, exe_05_25_35;
+    std::vector<double> fit_05_25_35;
+    ifstream in_05_25_35("9C_Reg1_0.5-_2.5-_3.5-.out");
+    while(in_05_25_35 >> var1 >> var2 >> var3 >> var4 >> var5 >> var6 >> var7 >> var8 >> var9) {
+        cm_05_25_35.push_back(var1);
+        exe_05_25_35.push_back(var2);
+        fit_05_25_35.push_back(var4);
+    }
+    in_05_25_35.close();
+
+    TCanvas* c = new TCanvas("c");
+    int size = cm_05_25_05_35.size();
+
+    TGraphErrors *region1 = new TGraphErrors();
+
+    TGraph *gr_05_25_05_35 = new TGraph();
+    TGraph *gr_05_25_15 = new TGraph();
+    TGraph *gr_05_25_15_35 = new TGraph();
+    TGraph *gr_05_25_25_35 = new TGraph();
+    TGraph *gr_05_25_25 = new TGraph();
+    TGraph *gr_05_25_35 = new TGraph();
+
+    for(int i = 0; i < size; i++) {
+        region1->SetPoint(i, cm_05_25_05_35[i] + 1.3, cs_05_25_05_35[i]*1000.);
+        region1->SetPointError(i, 0, cserr_05_25_05_35[i]*1000.);
+
+        gr_05_25_05_35->SetPoint(i, cm_05_25_05_35[i] + 1.3, fit_05_25_05_35[i]*1000.);
+
+        gr_05_25_15->SetPoint(i, cm_05_25_15[i] + 1.3, fit_05_25_15[i]*1000.);
+
+        gr_05_25_15_35->SetPoint(i, cm_05_25_15_35[i] + 1.3, fit_05_25_15_35[i]*1000.);
+
+        gr_05_25_25_35->SetPoint(i, cm_05_25_25_35[i] + 1.3, fit_05_25_25_35[i]*1000.);
+
+        gr_05_25_25->SetPoint(i, cm_05_25_25[i] + 1.3, fit_05_25_25[i]*1000.);
+
+        gr_05_25_35->SetPoint(i, cm_05_25_35[i] + 1.3, fit_05_25_35[i]*1000.);
+    }
+
+    region1->SetMarkerStyle(20);
+    region1->SetTitle("; Excitation Energy [MeV]; Cross Section [mb/sr]");
+    region1->GetXaxis()->CenterTitle();
+    region1->GetYaxis()->CenterTitle();
+    region1->SetMinimum(0);
+    region1->SetMaximum(160);
+    region1->Draw("APE");
+
+    TLatex latex;
+    latex.DrawLatex(2, 140., "155^{o}-170^{o}");
+
+    // c->Print("9C_Region1_CS.pdf");
+
+    //graph 1
+
+    // // 05_25_15
+    // gr_05_25_15->SetLineStyle(1);  gr_05_25_15->SetLineColor(1); gr_05_25_15->SetLineWidth(4);
+    // gr_05_25_15->Draw("PCsame");
+
+    // // 05_25_25
+    // gr_05_25_25->SetLineStyle(9); gr_05_25_25->SetLineColor(3); gr_05_25_25->SetLineWidth(4);
+    // gr_05_25_25->Draw("PCsame");
+
+    // // 05_25_35
+    // gr_05_25_35->SetLineStyle(10); gr_05_25_35->SetLineColor(4); gr_05_25_35->SetLineWidth(4);
+    // gr_05_25_35->Draw("PCsame");
+
+    // // 05_25_25_35
+    // gr_05_25_25_35->SetLineStyle(2); gr_05_25_25_35->SetLineColor(2); gr_05_25_25_35->SetLineWidth(5);
+    // gr_05_25_25_35->Draw("PCsame");
+
+    // c->Print("9C_Region1_CS_graph1.pdf");
+
+    // graph 2
+
+    // 05_25_25_35
+    gr_05_25_25_35->SetLineStyle(2); gr_05_25_25_35->SetLineColor(2); gr_05_25_25_35->SetLineWidth(5);
+    gr_05_25_25_35->Draw("PCsame");
+
+    // 05_25_15_35
+    gr_05_25_15_35->SetLineStyle(9); gr_05_25_15_35->SetLineColor(3); gr_05_25_15_35->SetLineWidth(4);
+    gr_05_25_15_35->Draw("PCsame");
+
+    // // 05_25_05_35
+    // gr_05_25_05_35->SetLineStyle(1); gr_05_25_05_35->SetLineColor(4); gr_05_25_05_35->SetLineWidth(4);
+    // gr_05_25_05_35->Draw("PCsame");
+
+    c->Print("9C_Region1_CS_graph2.pdf");
+}
